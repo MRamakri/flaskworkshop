c6737391f48ed7fb5ee221e142d615d223ddd9f6
Manikandan Ramakrishnan <manikandan.ramakrishnan@hotmail.com>
2016-03-21 02:12:42 -0400

    Add links to different pages

--- app.py
+++ app.py
@@ -3,11 +3,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+questions = ['Is it compiled?', 'Does it run in a VM?']
 guesses = ['Python', 'Java', 'C++']
 
 @app.route('/')
 def index():
     return render_template('index.html') 
+
+@app.route("/question/<int:id>")
+def question(id):
+    return render_template('question.html', question=questions[id])
 
 @app.route('/guess/<int:id>')
 def guess(id,):
