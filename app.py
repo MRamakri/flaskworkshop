d4ab603910b29e72fb3b85e0eb1409aeb6a536cf
Manikandan Ramakrishnan <manikandan.ramakrishnan@hotmail.com>
2016-03-21 01:03:21 -0400

    v0.2 Choice of python,java,c++

---
+++ app.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+guesses = ['Python', 'Java', 'C++']
+
+@app.route('/')
+def index():
+    return '<h1>Guess the Language!</h1>'
+
+@app.route('/guess/<int:id>')
+def guess(id,):
+    return ('<h1>Guess the Language!</h1>'
+            '<p>My Guess: {0}</p>').format(guesses[id])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
