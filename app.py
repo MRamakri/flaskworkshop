3718bba41fdf0ac3c3a6e2335f7e83de951a8033
MRamakri <manikandan.ramakrishnan@inmar.com>
2016-03-22 16:36:30 -0400

    Html pages for different screens

--- app.py
+++ app.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 questions = ['Is it compiled?', 'Does it run in a VM?']
 guesses = ['Python', 'Java', 'C++']
 
+
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
 
 @app.route("/question/<int:id>")
 def question(id):
     return render_template('question.html', question=questions[id])
 
+
 @app.route('/guess/<int:id>')
-def guess(id,):
-    return render_template('guess.html', guess=guesses[id]) 
+def guess(id, ):
+    return render_template('guess.html', guess=guesses[id])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
